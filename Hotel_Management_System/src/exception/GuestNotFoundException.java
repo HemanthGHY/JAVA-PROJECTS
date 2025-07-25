73b84750d7aad753c67df905af6dfc186a980d89
HemanthGHY <gaddamhemanthyadav@gmail.com>
2025-07-25 12:31:55 +0530

    Revert "initial update"
    This reverts commit e66b39bfd28c82ec3d59cd0c677ed2c54692d514.

--- Hotel_Management_System/src/exception/GuestNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class GuestNotFoundException extends Exception {
-    public GuestNotFoundException(String message) {
-        super(message);
-    }
-}
--- Hotel_Management_System/src/exception/RoomNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class RoomNotAvailableException extends Exception {
-    public RoomNotAvailableException(String message) {
-        super(message);
-    }
-}
